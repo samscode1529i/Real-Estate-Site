8faa9a5294b0e7fbfb076513b5dcf02cf506c110
Samuel Irenikase <samscode1529i@gmail.com>
2026-01-01 17:29:51 +0100

    navbar completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
+import logo from '../assets/logo2.png'
 
 const Navbar = () => {
   return (
-    <div>
-        <div>
-            
+    <nav className='w-full shadow-sm' >
+
+        <div className="max-w-7xl px-29 py-10 h-16 flex items-center justify-between ">
+
+          <div>
+            {/* left side */}
+            <img src={logo} alt="logo"/>
+          </div>
+
+          {/* Right Side */}
+          <div className="flex space-x-8 font-medium">
+            <a href="#" className='cursor-pointer hover:text-amber-300 transition'>Home</a>
+            <a href="#" className='cursor-pointer hover:text-amber-300 transition'>Listings</a>
+            <a href="#" className='cursor-pointer hover:text-amber-300 transition'>About</a>
+            <a href="#" className='cursor-pointer hover:text-amber-300 transition'>Contact Us</a>
+          </div>
+
+          <div>
+            <button className='cursor-pointer bg-amber-300 px-5 py-1 rounded-tr-[13px]'>
+              Work with us
+            </button>
+          </div>
         </div>
-    </div>
+
+    </nav>
   )
 }
 
